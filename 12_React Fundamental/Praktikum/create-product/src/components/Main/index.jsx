694582f9f5e84d00a24a142d1c1315ec8c9adbe4
labit-riset-umm <71822965+alifftwa@users.noreply.github.com>
2023-09-20 18:15:51 +0700

    update-react

---
+++ 12_React Fundamental/Praktikum/create-product/src/components/Main/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div class="d-flex flex-column justify-content-center align-items-center">
+      <img
+        src="./asset/bootstrap-logo.svg.png"
+        alt="Bootstrap"
+        width="50"
+        height="42"
+      />
+      <h2 class="my-4">Create Account</h2>
+      <p class="w-50 text-center">
+        Below is an example form built entirely with Bootstrap’s form controls.
+        Each required form group has a validation state that can be triggered by
+        attempting to submit the form without completing it.
+      </p>
+    </div>
+  );
+}
+
+export default Main;
